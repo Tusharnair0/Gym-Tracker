e754bed1bab7ea3e3054f2870b6ea2874927f192
Tushar <1tusharnair@gmail.com>
2025-07-20 20:06:06 +0530

    feat : Add update & delete Endpoints

--- src/main/groovy/com/springboot/Project/Gym/Tracker/Controllers/GymExerciseController.java
+++ src/main/groovy/com/springboot/Project/Gym/Tracker/Controllers/GymExerciseController.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/exercises")
 public class GymExerciseController {
