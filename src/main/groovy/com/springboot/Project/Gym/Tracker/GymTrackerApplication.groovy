b15502c26601bbab0e1b7071a32339a1136c48d0
Tushar <1tusharnair@gmail.com>
2025-07-20 11:59:13 +0530

    Initial commit : Project setup

---
+++ src/main/groovy/com/springboot/Project/Gym/Tracker/GymTrackerApplication.groovy
@@ -0,0 +1,13 @@
+package com.springboot.Project.Gym.Tracker
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class GymTrackerApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(GymTrackerApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/springboot/Project/Gym/Tracker/GymTrackerApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.springboot.Project.Gym.Tracker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GymTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
